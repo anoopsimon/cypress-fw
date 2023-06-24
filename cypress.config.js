d4fa785302a9cb18c416d2d43ae1ed7e3ace2e0a
Anoop Simon <Anoop.Simon@health.vic.gov.au>
2023-06-24 15:44:41 +1000

    first commit

---
+++ cypress.config.js
@@ -0,0 +1,28 @@
+const { defineConfig } = require('cypress')
+const fs = require('fs')
+
+module.exports = defineConfig({
+  // setupNodeEvents can be defined in either
+  // the e2e or component configuration
+  e2e: {
+    setupNodeEvents(on, config) {
+      on('after:screenshot', (details) => {
+        console.log('Capturing screenshot') // print all details to terminal
+
+        const newPath = 'cypress/downloads/screenshot.png'
+
+        return new Promise((resolve, reject) => {
+          // fs.rename moves the file to the existing directory 'new/path/to'
+          // and renames the image to 'screenshot.png'
+          fs.rename(details.path, newPath, (err) => {
+            if (err) return reject(err)
+
+            // because we renamed and moved the image, resolve with the new path
+            // so it is accurate in the test results
+            resolve({ path: newPath })
+          })
+        })
+      })
+    },
+  },
+})
