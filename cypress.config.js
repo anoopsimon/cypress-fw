50093d474e449b6f493b73f169efa5d00a36fe45
Anoop Simon <Anoop.Simon@health.vic.gov.au>
2023-06-24 22:30:20 +1000

    Added additional configuration options for mocha awesome

--- cypress.config.js
+++ cypress.config.js
@@ -4,29 +4,35 @@ const fs = require('fs')
 
 module.exports = defineConfig({
   reporter: 'cypress-mochawesome-reporter',
-
+  reporterOptions: {
+    charts: true,
+    reportPageTitle: 'EndToEnd Test',
+    embeddedScreenshots: true,
+    inlineAssets: true,
+    saveAllAttempts: false,
+  },
   // setupNodeEvents can be defined in either
   // the e2e or component configuration
   e2e: {
     setupNodeEvents(on, config) {
       require('cypress-mochawesome-reporter/plugin')(on);
-      on('after:screenshot', (details) => {
-        console.log('Capturing screenshot') // print all details to terminal
+      // on('after:screenshot', (details) => {
+      //   console.log('Capturing screenshot') // print all details to terminal
 
-        const newPath = 'cypress/reports/screenshot.png'
+      //   const newPath = 'screenshot.png'
 
-        return new Promise((resolve, reject) => {
-          // fs.rename moves the file to the existing directory 'new/path/to'
-          // and renames the image to 'screenshot.png'
-          fs.rename(details.path, newPath, (err) => {
-            if (err) return reject(err)
+      //   return new Promise((resolve, reject) => {
+      //     // fs.rename moves the file to the existing directory 'new/path/to'
+      //     // and renames the image to 'screenshot.png'
+      //     fs.rename(details.path, newPath, (err) => {
+      //       if (err) return reject(err)
 
-            // because we renamed and moved the image, resolve with the new path
-            // so it is accurate in the test results
-            resolve({ path: newPath })
-          })
-        })
-      })
+      //       // because we renamed and moved the image, resolve with the new path
+      //       // so it is accurate in the test results
+      //       resolve({ path: newPath })
+      //     })
+      //   })
+      // })
     },
   },
 })
---
+++ cypress/e2e/visa-registration.cy.js
@@ -0,0 +1,41 @@
+import locators from '../fixtures/locators.json'
+
+describe('Visa Registration Form Tests', () => {
+  
+  it('Fill visa application form and validates success message', () => {
+    cy.visit('cypress/test.html');
+
+    cy.get(locators.fullNameInput).clear().type('John Doe');
+    cy.get(locators.passportNumberInput).clear().type('ABC123456');
+    cy.get(locators.countrySelect).select('USA');
+    cy.get(locators.purposeTextArea).clear().type('Tourism');
+    cy.get(locators.genderMaleRadio).click({ force: true });
+    cy.get(locators.visaTypeTouristCheckbox).click({ force: true });
+
+    cy.get(locators.submitButton).click();
+
+    cy.get(locators.successMessage)
+      .should('be.visible')
+      .should('have.css', 'background-color', 'rgb(223, 240, 216)')
+      .should('contain', 'Registration successful!');
+      cy.screenshot();
+
+    });
+
+    it('Should display all the HTML controls', () => {
+      cy.visit('cypress/test.html');
+
+      // Verify the visibility of each control using locators
+      cy.get(locators.fullNameInput).should('be.visible');
+      cy.get(locators.passportNumberInput).should('be.visible');
+      cy.get(locators.countrySelect).should('be.visible');
+      cy.get(locators.purposeTextArea).should('be.visible');
+      cy.get(locators.genderMaleRadio).should('be.visible');
+      cy.get(locators.genderFemaleRadio).should('be.visible');
+      cy.get(locators.visaTypeTouristCheckbox).should('be.visible');
+      cy.get(locators.visaTypeBusinessCheckbox).should('be.visible');
+      cy.get(locators.submitButton).should('be.visible');
+      cy.screenshot();
+    });
+
+});
